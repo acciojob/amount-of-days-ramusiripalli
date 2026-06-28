059ec40ed7eb96504509bdbf8c25119d64c32ac8
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-28 14:59:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+function isLeapYear(year){
+if(year%400 === 0) return true;
+else if(year%100 === 0) return false;
+else if(year%4 === 0) return true;
+else return false;
+
+}
+
+function daysOfAYear(year){
+	if(isLeapYear(year)){
+return 366;
+	}
+	else{
+return 365;
+	}
+}
+
+
+
+daysOfAYear(2024);
